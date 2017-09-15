89e3f1be38bb806f6ab924939c5f6fddfd3cf615
Luis Castro <luiscastror9@gmail.com>
2017-09-15 19:17:06 -0300

    Commit Inicial
    Generacion Proyecto

---
+++ IntiveFDV-Test/IntiveFDV-Test/Rentals.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntiveFDV_Test
+{
+    public class Rental
+    {
+        public int idRental { get; set; }
+        public TypeRental TypeRental { get; set; }
+
+    }
+    public class TypeRental
+    {
+        public int idTypeRental { get; set; }
+        public TimeRental TimeRental { get; set; }
+        public double pricTypeRental { get; set; }
+
+    }
+    public class TimeRental
+    {
+        public int idTimeRental { get; set; }
+        public string descTimeRental { get; set; }
+
+    }
+    public class FamilyRental
+    {
+        public int idTimeRental { get; set; }
+        public List<Rental> Rentals{ get; set; }
+        public double discountFamilyRental { get; set; }
+    }
+}
