a3c9ab65d245138197db8d6bbcb2936c421ae776
Luis Castro <luiscastror9@gmail.com>
2017-09-17 13:53:18 -0300

    Commit Final
    Commit Final de Modelo y Test Unitario con casos de uso

--- IntiveFDV-Test/IntiveFDV-Test/Rentals.cs
+++ IntiveFDV-Test/IntiveFDV-Test/Rentals.cs
@@ -8,27 +8,98 @@ namespace IntiveFDV_Test
 {
     public class Rental
     {
-        public int idRental { get; set; }
-        public TypeRental TypeRental { get; set; }
+        private static Rental instance = null;
 
-    }
-    public class TypeRental
-    {
-        public int idTypeRental { get; set; }
-        public TimeRental TimeRental { get; set; }
-        public double pricTypeRental { get; set; }
+        public Guid idRental { get; set; }
+        public DateTime startdateRental { get; set; }
+        public DateTime enddateRental { get; set; }
+        public const double hourTimeRentalPrice = 5;
+        public const double dayTimeRentalPrice = 20;
+        public const double weekTimeRentalPrice = 60;
 
-    }
-    public class TimeRental
-    {
-        public int idTimeRental { get; set; }
-        public string descTimeRental { get; set; }
+        public double totalRental { get; set; }
 
+        public static Rental GetInstance()
+        {
+            if (instance == null)
+                instance = new Rental();
+
+            return instance;
+        }
+        public Rental RentTotal(DateTime start,DateTime end)
+        {
+
+            Rental Rent = new Rental();
+            Rent.idRental = new Guid();
+            TimeSpan diff = end.Subtract(start);
+            //DateTime dt = Convert.ToDateTime(diff.ToString());
+            Rent.startdateRental = start;
+            Rent.enddateRental = end;
+            Rent.totalRental= Convert.ToDouble(Rent.RentalByDays(diff.Days).totalRental + Rent.RentalByHour(diff.Hours).totalRental + (diff.Days >= 7 ? Rent.RentalByWeeks(diff.Days / 7).totalRental : 0));
+            return Rent;
+        }
+
+        public Rental RentalByHour(int Hours)
+        {
+            Rental Rent = new Rental();
+            Rent.totalRental = Hours * hourTimeRentalPrice;
+
+            return Rent;
+        }
+        public Rental RentalByDays(int Days)
+        {
+            Rental Rent = new Rental();
+            Rent.totalRental = Days * dayTimeRentalPrice;
+
+            return Rent;
+        }
+        public Rental RentalByWeeks(int Weeks)
+        {
+            Rental Rent = new Rental();
+            Rent.totalRental = Weeks * weekTimeRentalPrice;
+
+            return Rent;
+        }
     }
     public class FamilyRental
     {
-        public int idTimeRental { get; set; }
-        public List<Rental> Rentals{ get; set; }
-        public double discountFamilyRental { get; set; }
+        private static FamilyRental instance = null;
+
+        private const int MinLimit = 3;
+        private const int MaxLimit = 5;
+
+        public int idFamilyRental { get; set; }
+        public List<Rental> Rentals { get; set; }
+        public double discountRental { get; set; }
+
+        public double totalRental { get; set; }
+
+        public static FamilyRental GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new FamilyRental();
+                instance.Rentals = new List<Rental>();
+            }
+            return instance;
+        }
+
+        public double GetDiscount(ICollection<Rental> Rentals)
+        {
+            double discount = 0;
+            const int discountPercentFamilyRental = 30;
+            if (Rentals.Count() > MinLimit && Rentals.Count() <= MaxLimit)
+                discount = Rentals.Sum(z => Convert.ToDouble(z.totalRental))*discountPercentFamilyRental /100;
+            
+            return discount;
     }
+    }
+
+    
+
+   
+   
 }
+
+   
+
--- IntiveFDV-Test/IntiveFDV-Test_UT/UnitTest1.cs
+++ IntiveFDV-Test/IntiveFDV-Test_UT/UnitTest1.cs
@@ -1,14 +1,79 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace IntiveFDV_Test_UT
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTestIntiveFDV_Test
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethodBasic()
         {
+            IntiveFDV_Test.Rental Rent = new IntiveFDV_Test.Rental();
+         
+        }
+
+        [TestMethod]
+        public void TestMethodDate()
+        {
+            DateTime startdate = new DateTime(2017, 09, 15, 09, 15, 25);
+            DateTime enddate = new DateTime(2017, 09, 30, 09, 15, 25);
+
+
+            double rs;
+            IntiveFDV_Test.Rental Rent = new IntiveFDV_Test.Rental();
+            TimeSpan diff = enddate.Subtract(startdate);
+            //DateTime dt = Convert.ToDateTime(diff.ToString());
+            rs = Convert.ToDouble(Rent.RentalByDays(diff.Days).totalRental+ Rent.RentalByHour(diff.Hours).totalRental + (diff.Days>=7 ? Rent.RentalByWeeks(diff.Days / 7).totalRental : 0) );
+            //Assert.AreEqual(60, rs);
+            System.Diagnostics.Debug.WriteLine(rs);
+
+            //return rs;
+        }
+        [TestMethod]
+        public void TestMethodDateDetailed()
+        {
+            DateTime startdate = new DateTime(2017, 09, 15, 09, 15, 25);
+            DateTime enddate = new DateTime(2017, 09, 30, 09, 15, 25);
+
+
+            double rs;
+            IntiveFDV_Test.Rental Rent = new IntiveFDV_Test.Rental().RentTotal(startdate, enddate);
+           // Rent.RentTotal(startdate, enddate);
+            System.Diagnostics.Debug.WriteLine("Rental ID:"+Rent.idRental);
+            System.Diagnostics.Debug.WriteLine("Rental Start Date:" + Rent.startdateRental);
+            System.Diagnostics.Debug.WriteLine("Rental End Date:" + Rent.enddateRental);
+            System.Diagnostics.Debug.WriteLine("Rental Total:" + Rent.totalRental);
+
+            //return rs;
+        }
+
+        [TestMethod]
+        public void TestMethodTotalDiscount()
+        {
+            DateTime startdate = new DateTime(2017, 09, 15, 09, 15, 25);
+            DateTime enddate = new DateTime(2017, 09, 30, 09, 15, 25);
+
+            IntiveFDV_Test.FamilyRental fR =  IntiveFDV_Test.FamilyRental.GetInstance();
+            IntiveFDV_Test.Rental Rent = new IntiveFDV_Test.Rental().RentTotal(startdate, enddate);
+            fR.Rentals.Add(Rent);
+            //fR.Rentals.Add(Rent);
+            //fR.Rentals.Add(Rent);
+            fR.Rentals.Add(Rent);
+            fR.discountRental = IntiveFDV_Test.FamilyRental.GetInstance().GetDiscount(fR.Rentals);
+            fR.totalRental =fR.Rentals.Sum(z => Convert.ToDouble(z.totalRental))-fR.discountRental;
+            System.Diagnostics.Debug.WriteLine("Family Rental ID:" + fR.idFamilyRental);
+
+            foreach (IntiveFDV_Test.Rental rnt in fR.Rentals)
+            {
+                System.Diagnostics.Debug.WriteLine("Rental ID:" + rnt.idRental);
+                System.Diagnostics.Debug.WriteLine("Rental Start Date:" + rnt.startdateRental);
+                System.Diagnostics.Debug.WriteLine("Rental End Date:" + rnt.enddateRental);
+                System.Diagnostics.Debug.WriteLine("Rental Total:" + rnt.totalRental);
+            }
+            System.Diagnostics.Debug.WriteLine("Family Rental Discount:" + fR.discountRental);
+            System.Diagnostics.Debug.WriteLine("Family Rental Total:" + fR.totalRental);
         }
     }
 }
